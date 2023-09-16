9d12dc6dcd379da88037ef44beb4c919a824d181
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-09-16 17:24:00 +0700

    feat(splash screen): added splash screen

--- app/src/main/java/com/example/tiroberita/ui/activities/onboard/OnBoardActivitiy.java
+++ app/src/main/java/com/example/tiroberita/ui/activities/onboard/OnBoardActivitiy.java
@@ -2,7 +2,10 @@ package com.example.tiroberita.ui.activities.onboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import com.example.tiroberita.R;
 
@@ -12,5 +15,13 @@ public class OnBoardActivitiy extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_board_activitiy);
+
+        // full screen
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+
+
     }
 }
--- app/src/main/java/com/example/tiroberita/ui/activities/splash/SplashScreenActivity.java
+++ app/src/main/java/com/example/tiroberita/ui/activities/splash/SplashScreenActivity.java
@@ -2,9 +2,13 @@ package com.example.tiroberita.ui.activities.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import com.example.tiroberita.R;
+import com.example.tiroberita.ui.activities.onboard.OnBoardActivitiy;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -12,5 +16,19 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // full screen
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, OnBoardActivitiy.class));
+                finish();
+            }
+        }, 1800);
     }
 }
