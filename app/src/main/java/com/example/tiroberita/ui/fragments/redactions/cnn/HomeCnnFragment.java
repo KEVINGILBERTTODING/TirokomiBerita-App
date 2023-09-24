f55ef539bc845e822346b6e9139fbbe7679620b2
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-09-24 16:09:16 +0700

    Feat(filter): added filter by post title

--- app/src/main/java/com/example/tiroberita/ui/fragments/redactions/cnn/HomeCnnFragment.java
+++ app/src/main/java/com/example/tiroberita/ui/fragments/redactions/cnn/HomeCnnFragment.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Handler;
@@ -20,6 +22,7 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -93,6 +96,8 @@ public class HomeCnnFragment extends Fragment implements ItemClickListener {
         setUpBottomSheetShare();
         setUpBottomSheetWebView();
         setUpBottomSheetMediaBerita();
+        hideFab();
+
 
         binding.tvUsername.setText(sharedPreferences.getString(Constans.USERNAME, "-"));
 
@@ -649,8 +654,69 @@ public class HomeCnnFragment extends Fragment implements ItemClickListener {
             @Override
             public void onRefresh() {
                 getData(newsType);
+                binding.searchView.setQuery("", false);
+                binding.searchView.setIconified(true);
             }
         });
+
+
+
+        binding.btnShare.setOnClickListener(view -> {
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, postUrl);
+            sendIntent.setType("text/plain");
+
+            // Hapus preferensi pilihan pengguna (default)
+            sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+
+            // Membuat chooser dialog
+            Intent shareIntent = Intent.createChooser(sendIntent, null);
+            startActivity(shareIntent);
+        });
+
+        binding.btnSearch.setOnClickListener(view -> {
+            binding.searchView.setVisibility(View.VISIBLE);
+            binding.searchView.setIconified(false);
+            binding.btnSearch.setVisibility(View.GONE);
+            binding.lrHeader.setVisibility(View.GONE);
+        });
+
+
+
+        binding.searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                binding.lrHeader.setVisibility(View.VISIBLE);
+                binding.btnSearch.setVisibility(View.VISIBLE);
+                binding.searchView.setVisibility(View.GONE);
+                binding.lrHeader.setVisibility(View.VISIBLE);
+
+
+                return false;
+            }
+        });
+
+        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filter(newText);
+                return false;
+            }
+        });
+
+
+
+        binding.fabMediaPicker.setOnClickListener(view -> {
+            showBottomSheetMediaBerita();
+            binding.fabMediaPicker.setVisibility(View.GONE);
+        });
+
 
         binding.vOverlay.setOnClickListener(view -> {
             hideBottomSheetShare();
@@ -678,9 +744,7 @@ public class HomeCnnFragment extends Fragment implements ItemClickListener {
             savePost();
 
         });
-        binding.searchView.setOnClickListener(view -> {
-            Toasty.normal(getContext(), "Berhasul", Toasty.LENGTH_SHORT).show();
-        });
+
 
         binding.tvHeader.setOnClickListener(view -> {
             setWebView(Constans.URL_CNN_INDONESIA);
@@ -871,6 +935,7 @@ public class HomeCnnFragment extends Fragment implements ItemClickListener {
         bottomSheetMediaBerita.setState(BottomSheetBehavior.STATE_HIDDEN);
         binding.vOverlay.setVisibility(View.GONE);
         bottomSheetMediaBerita.setPeekHeight(0);
+        binding.fabMediaPicker.setVisibility(View.VISIBLE);
 
     }
 
@@ -878,6 +943,7 @@ public class HomeCnnFragment extends Fragment implements ItemClickListener {
         bottomSheetMediaBerita.setState(BottomSheetBehavior.STATE_EXPANDED);
         bottomSheetMediaBerita.setPeekHeight(600);
         binding.vOverlay.setVisibility(View.VISIBLE);
+        binding.fabMediaPicker.setVisibility(View.GONE);
 
 
     }
@@ -1022,6 +1088,44 @@ public class HomeCnnFragment extends Fragment implements ItemClickListener {
 
 
     }
+
+    private void hideFab() {
+
+        binding.rvNews.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx,
+                                   int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (dy > 0) {
+                    binding.fabMediaPicker.hide();
+                } else if (dy < 0) {
+                    binding.fabMediaPicker.show();
+                }
+            }
+        });
+    }
+
+
+    private void filter(String querry) {
+        ArrayList<PostModel> filteredList = new ArrayList<>();
+        for (PostModel item : postModelList) {
+            if (item != null && item.getTitle().toLowerCase().contains(querry.toLowerCase())) {
+                filteredList.add(item);
+            }
+
+            newsAdapter.filter(filteredList);
+
+            if (filteredList.isEmpty()) {
+                binding.lrEmpty.setVisibility(View.VISIBLE);
+                binding.tvMessage.setText("Berita tidak ditemukan.");
+            }else {
+                newsAdapter.filter(filteredList);
+                binding.lrEmpty.setVisibility(View.GONE);
+            }
+        }
+    }
+
     private void moveFragment(Fragment fragment) {
         getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameHome, fragment)
                 .addToBackStack(null).commit();
